0e14d7b0f472f53b4b5bc3cf2c7b5374c672be57
Andrea Rozza <andre.sixx.94@gmail.com>
2019-09-19 01:42:41 +0200

    haarg

--- src/parallel/parallel_threshold.cpp
+++ src/parallel/parallel_threshold.cpp
@@ -6,22 +6,7 @@ void p_threshold(int m, int n, double u[], int n_level, double threshold)
 	int level = (int)pow(2, n_level);
 	int l = (int)(m / level);
 	int k = (int)(n / level);
-/*
-	#pragma omp parallel for private(i, j) shared(u, threshold) collapse(2)
-	for (i = 0; i < m; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			if (!(i < l && j < k))
-			{
-				if (abs(u[j + i * n]) <= threshold)
-				{
-					u[j + i * n] = 0.0;
-				}
-			}
-		}
-	}
-*/
+
 	#pragma omp parallel for private(i, j) shared(u, threshold) collapse(2)
 	for (i = 0; i < l; i++)
 	{
@@ -55,19 +40,7 @@ double p_mean(int m, int n, double u[], int n_level)
 	double sum = 0;
 	double mean = 0;
 	int n_element = (m * n) - (l * k);
-/*
-	#pragma omp parallel for private(i, j) shared(u) reduction(+:sum) collapse(2)
-	for (i = 0; i < m; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			if (!(i < l && j < k))
-			{
-				sum += abs(u[j + i * n]);
-			}
-		}
-	}
-*/
+
 	#pragma omp parallel for private(i, j) shared(u) reduction(+:sum) collapse(2)
 	for (i = 0; i < l; i++)
 	{
